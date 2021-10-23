41e8e572d3c8ec97ae16f7ea173a5938c3b6273e
MS2 <marlisumarli@outlook.com>
2021-10-24 01:55:07 +0700

    Add project files.

---
+++ Popapp/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Popapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.guna2Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.Btn1 = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.Btn2 = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.Btn3 = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.Btn4 = new Guna.UI2.WinForms.Guna2GradientButton();
+            this.guna2Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Bauhaus 93", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(58, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "popup!";
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.AutoRoundedCorners = true;
+            this.guna2Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(9)))), ((int)(((byte)(42)))));
+            this.guna2Panel1.BorderRadius = 29;
+            this.guna2Panel1.Controls.Add(this.button1);
+            this.guna2Panel1.Controls.Add(this.guna2Panel2);
+            this.guna2Panel1.Controls.Add(this.pictureBox1);
+            this.guna2Panel1.Controls.Add(this.label1);
+            this.guna2Panel1.Location = new System.Drawing.Point(-6, -4);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.ShadowDecoration.Parent = this.guna2Panel1;
+            this.guna2Panel1.Size = new System.Drawing.Size(825, 61);
+            this.guna2Panel1.TabIndex = 3;
+            this.guna2Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.guna2Panel1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Popapp.Properties.Resources.cancel_48px;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(777, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(34, 31);
+            this.button1.TabIndex = 5;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // guna2Panel2
+            // 
+            this.guna2Panel2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.guna2Panel2.Location = new System.Drawing.Point(154, 5);
+            this.guna2Panel2.Name = "guna2Panel2";
+            this.guna2Panel2.ShadowDecoration.Parent = this.guna2Panel2;
+            this.guna2Panel2.Size = new System.Drawing.Size(4, 57);
+            this.guna2Panel2.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Popapp.Properties.Resources.natural_user_interface_2_64px;
+            this.pictureBox1.Location = new System.Drawing.Point(9, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(43, 53);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.BorderRadius = 15;
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.DockIndicatorColor = System.Drawing.Color.Transparent;
+            // 
+            // Btn1
+            // 
+            this.Btn1.BorderRadius = 18;
+            this.Btn1.CheckedState.Parent = this.Btn1;
+            this.Btn1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Btn1.CustomImages.Parent = this.Btn1;
+            this.Btn1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.Btn1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.Btn1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.Btn1.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.Btn1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.Btn1.DisabledState.Parent = this.Btn1;
+            this.Btn1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(230)))), ((int)(((byte)(185)))));
+            this.Btn1.FillColor2 = System.Drawing.Color.SeaGreen;
+            this.Btn1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(4)))), ((int)(((byte)(45)))), ((int)(((byte)(9)))));
+            this.Btn1.HoverState.Parent = this.Btn1;
+            this.Btn1.Location = new System.Drawing.Point(109, 167);
+            this.Btn1.Name = "Btn1";
+            this.Btn1.ShadowDecoration.Parent = this.Btn1;
+            this.Btn1.Size = new System.Drawing.Size(234, 70);
+            this.Btn1.TabIndex = 4;
+            this.Btn1.Text = "nama saya";
+            this.Btn1.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
+            this.Btn1.Tile = true;
+            this.Btn1.Click += new System.EventHandler(this.Btn1_Click);
+            // 
+            // Btn2
+            // 
+            this.Btn2.BorderRadius = 18;
+            this.Btn2.CheckedState.Parent = this.Btn2;
+            this.Btn2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Btn2.CustomImages.Parent = this.Btn2;
+            this.Btn2.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.Btn2.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.Btn2.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.Btn2.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.Btn2.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.Btn2.DisabledState.Parent = this.Btn2;
+            this.Btn2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(9)))), ((int)(((byte)(66)))));
+            this.Btn2.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.Btn2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn2.ForeColor = System.Drawing.Color.Snow;
+            this.Btn2.HoverState.Parent = this.Btn2;
+            this.Btn2.Location = new System.Drawing.Point(109, 307);
+            this.Btn2.Name = "Btn2";
+            this.Btn2.ShadowDecoration.Parent = this.Btn2;
+            this.Btn2.Size = new System.Drawing.Size(234, 70);
+            this.Btn2.TabIndex = 4;
+            this.Btn2.Text = "kelas saya";
+            this.Btn2.Click += new System.EventHandler(this.Btn2_Click);
+            // 
+            // Btn3
+            // 
+            this.Btn3.BorderRadius = 18;
+            this.Btn3.CheckedState.Parent = this.Btn3;
+            this.Btn3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Btn3.CustomImages.Parent = this.Btn3;
+            this.Btn3.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.Btn3.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.Btn3.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.Btn3.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.Btn3.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.Btn3.DisabledState.Parent = this.Btn3;
+            this.Btn3.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(230)))), ((int)(((byte)(9)))), ((int)(((byte)(153)))));
+            this.Btn3.FillColor2 = System.Drawing.Color.Red;
+            this.Btn3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn3.ForeColor = System.Drawing.Color.White;
+            this.Btn3.HoverState.Parent = this.Btn3;
+            this.Btn3.Location = new System.Drawing.Point(427, 167);
+            this.Btn3.Name = "Btn3";
+            this.Btn3.ShadowDecoration.Parent = this.Btn3;
+            this.Btn3.Size = new System.Drawing.Size(234, 70);
+            this.Btn3.TabIndex = 4;
+            this.Btn3.Text = "alamat saya";
+            this.Btn3.Click += new System.EventHandler(this.Btn3_Click);
+            // 
+            // Btn4
+            // 
+            this.Btn4.BorderRadius = 18;
+            this.Btn4.CheckedState.Parent = this.Btn4;
+            this.Btn4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Btn4.CustomImages.Parent = this.Btn4;
+            this.Btn4.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.Btn4.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.Btn4.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.Btn4.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.Btn4.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.Btn4.DisabledState.Parent = this.Btn4;
+            this.Btn4.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(9)))), ((int)(((byte)(230)))));
+            this.Btn4.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(141)))), ((int)(((byte)(230)))));
+            this.Btn4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn4.ForeColor = System.Drawing.Color.Snow;
+            this.Btn4.HoverState.Parent = this.Btn4;
+            this.Btn4.Location = new System.Drawing.Point(427, 307);
+            this.Btn4.Name = "Btn4";
+            this.Btn4.ShadowDecoration.Parent = this.Btn4;
+            this.Btn4.Size = new System.Drawing.Size(234, 70);
+            this.Btn4.TabIndex = 4;
+            this.Btn4.Text = "sekian";
+            this.Btn4.Click += new System.EventHandler(this.Btn4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(54)))), ((int)(((byte)(90)))));
+            this.ClientSize = new System.Drawing.Size(817, 535);
+            this.Controls.Add(this.Btn4);
+            this.Controls.Add(this.Btn3);
+            this.Controls.Add(this.Btn2);
+            this.Controls.Add(this.Btn1);
+            this.Controls.Add(this.guna2Panel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Popup!";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private Guna.UI2.WinForms.Guna2GradientButton Btn4;
+        private Guna.UI2.WinForms.Guna2GradientButton Btn3;
+        private Guna.UI2.WinForms.Guna2GradientButton Btn2;
+        private Guna.UI2.WinForms.Guna2GradientButton Btn1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel2;
+        private System.Windows.Forms.Button button1;
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+    }
+}
+
---
+++ Popapp/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Popapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Alert(string msg, Form_Alert.enmType type)
+        {
+            Form_Alert frm = new Form_Alert();
+            frm.showAlert(msg,type);
+        }
+        
+        private void Btn1_Click(object sender, EventArgs e)
+        {
+           this.Alert("Sumarli", Form_Alert.enmType.Smrl);
+        }
+
+        private void Btn2_Click(object sender, EventArgs e)
+        {
+            this.Alert("Kelas IF21D", Form_Alert.enmType.Kls);
+        }
+
+        private void Btn3_Click(object sender, EventArgs e)
+        {
+            this.Alert("Subang Patokbeusi", Form_Alert.enmType.Almt);
+        }
+
+        private void Btn4_Click(object sender, EventArgs e)
+        {
+            this.Alert("Terimakasi!", Form_Alert.enmType.Skn);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Panel1_Paint(object sender, PaintEventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
